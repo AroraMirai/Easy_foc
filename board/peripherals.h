27d3bb10bd741edcbec1669a005ad0212bb73103
KM <98403576+AroraMirai@users.noreply.github.com>
2022-11-24 17:28:47 +0800

    Update peripherals.h
    Use CLOCK_GetFreq(kCLOCK_BusClk) function

--- board/peripherals.h
+++ board/peripherals.h
@@ -26,7 +26,7 @@ extern "C" {
 /* Definition of peripheral ID */
 #define FTM3_PERIPHERAL FTM3
 /* Definition of the clock source frequency */
-#define FTM3_CLOCK_SOURCE CLOCK_GetFreq(kCLOCK_BusClk)
+#define FTM3_CLOCK_SOURCE 47988736UL //CLOCK_GetFreq(kCLOCK_BusClk) 此处用getfreq函数就会出现问题，导致速度计算出现问题 不知道为什么
 /* Definition of the clock source frequency */
 #define FTM3_TIMER_MODULO_VALUE 99
 /* Definition of peripheral ID */
@@ -36,7 +36,7 @@ extern "C" {
 /* Definition of peripheral ID */
 #define FTM0_PERIPHERAL FTM0
 /* Definition of the clock source frequency */
-#define FTM0_CLOCK_SOURCE CLOCK_GetFreq(kCLOCK_BusClk)
+#define FTM0_CLOCK_SOURCE 47988736UL //CLOCK_GetFreq(kCLOCK_BusClk) 此处用getfreq函数就会出现问题，导致速度计算出现问题 不知道为什么
 /* Definition of the clock source frequency */
 #define FTM0_TIMER_MODULO_VALUE (((FTM0_CLOCK_SOURCE/ (1U << (FTM0_PERIPHERAL->SC & FTM_SC_PS_MASK))) / 500) - 1)
 /* FTM0 interrupt vector ID (number). */
